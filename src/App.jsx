e677c4455236d08edd6e7c3322c762276c581f61
PEDRO DA CONCEIÇÃO PIRES <pedro_c_pires@estudante.sesisenai.org.br>
2025-03-25 21:06:01 -0300

    Criação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
